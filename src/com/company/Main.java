856f0baec9dd7ea5d696dcd817bb8484577fe043
skunk45 <joel.bengtsson@elev.ga.ntig.se>
2019-11-07 11:13:55 +0100

    påbörjade projekt som ett proffs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
-
+import java.util.scanner;
+import java.util.random;
 public class Main {
 
     public static void main(String[] args) {
-	
+    String flerspelarläge;
+    String enspelarläge;
+    int computerint;
+
+    String response;
+
+    Scanner scan = new scanner(system.in);
+    random generator = new random();
+
+    System.out.println("sten sax påse din nisse");
+    System.out.println();
+
+    computerint = generator.nextInt(3)+1;
+    if (computerint == 1)
+        enspelarläge = "R";
+    else if (computerint == 2)
+        enspelarläge = "P"
     }
 }
